5514b277269ca015d729cdc9d0d16a3605601b21
John Njuguna <johnmnjuguna89@gmail.com>
2023-04-25 04:45:04 -0700

    task 0

---
+++ is_char_func.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * is_char - Entry point. Gets a char value and prints it.
+ * @print: input variadic variable
+ * Return: printed chars (success)
+*/
+int is_char(va_list print)
+{
+	char chars;
+	int num = 0;
+
+	chars = va_arg(print, int);
+
+	if (chars)
+	{
+		num = _putchar(chars);
+		return (num);
+	}
+
+	return (0);
+}
