a372a80c9ca668c909e6d5f3512daa311b1ba022
John Njuguna <johnmnjuguna89@gmail.com>
2023-04-25 04:49:48 -0700

    task 0: prints string value

---
+++ is_string_func.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * is_string - Entry point. Prints a string value.
+ * @print: input variadic variables
+ * Return: printed string || -1 if error (success)
+*/
+int is_string(va_list print)
+{
+	int num = 0;
+	char *str;
+
+	str = va_arg(print, char *);
+
+	if (!str)
+		return (-1);
+	else
+	{
+		num = _puts(str);
+		return (num);
+	}
+
+	return (0);
+}
